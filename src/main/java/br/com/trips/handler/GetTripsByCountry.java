6355062f36a0d6b7ab98d2d421d1a507aeadcbc7
Fabio Bara <fabio.bara@ze.delivery>
2021-10-16 04:23:06 -0300

    fix return code bug

--- src/main/java/br/com/trips/handler/GetTripsByCountry.java
+++ src/main/java/br/com/trips/handler/GetTripsByCountry.java
@@ -2,6 +2,7 @@ package br.com.trips.handler;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
@@ -25,12 +26,16 @@ public class GetTripsByCountry implements RequestHandler<HandlerRequest, Handler
 
 			tripDto = new TripDto(request);
 			Service service = new ServiceFactory().build(tripDto);
-			trips = service.findTrip(tripDto);			
+			trips = service.findTrip(tripDto);
 
 		} catch (Exception ex) {
 			return HandlerResponse.builder().setStatusCode(400).setRawBody(ex.getMessage()).build();
 		}
 
-		return HandlerResponse.builder().setStatusCode(200).setObjectBody(trips).build();
+		return HandlerResponse.builder().setStatusCode(verifyResponseCode.apply(trips)).setObjectBody(trips).build();
 	}
+
+	Function<List<Trip>, Integer> verifyResponseCode = x -> {
+		return x.size() == 0 ? 404 : 200;
+	};
 }
